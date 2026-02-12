2d4232807ba87627164a409377ebf80232b7f8c3
یونس لهرابی | CODE-NEST <97944070+YounesRezxer@users.noreply.github.com>
2026-02-12 13:13:49 +0330

    Add files via upload

---
+++ tamrin02.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+  
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            cout << "Vowel\n";
+        else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            cout << "Consonant\n";
+        else if (ch >= '0' && ch <= '9')
+            cout << "Digit\n";
+        else
+            cout << "Other character\n";
+
+    return 0;
+}
