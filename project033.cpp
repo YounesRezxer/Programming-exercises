39a03f79d0d4f9635d6840a88e9982d54611db71
یونس لهرابی | CODE-NEST <97944070+YounesRezxer@users.noreply.github.com>
2026-02-24 15:54:02 +0330

    Implement program to find maximum number in array

---
+++ project033.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxNum = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxNum)
+            maxNum = arr[i];
+    }
+
+    cout << "Max = " << maxNum;
+
+    return 0;
+}
